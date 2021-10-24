b4b623d443e6313205daf2c2ca021e808ce56395
azya_bug_off <azik-bagov@mail.ru>
2021-10-24 14:23:03 +0300

    lab04b done

--- lab04b/agg/src/main/scala/agg.scala
+++ lab04b/agg/src/main/scala/agg.scala
@@ -8,160 +8,60 @@ object agg{
 
   def main(args: Array[String]): Unit = {
 
-    val USER = "vladimir.cherny"
-    val IN_TOPIC = "vladimir_cherny"
-    val OUT_TOPIC = "vladimir_cherny_lab04b_out"
+    val spark = SparkSession
+      .builder
+      .appName("Lab04")
+      .getOrCreate()
 
-    val spark = SparkSession.builder.getOrCreate()
     import spark.implicits._
 
-//    def killAll() = {
-//      SparkSession
-//        .active
-//        .streams
-//        .active
-//        .foreach { x =>
-//          val desc = x.lastProgress.sources.head.description
-//          x.stop
-//          println(s"Stopped ${desc}")
-//        }
-//    }
-
-    val sdf = spark
-      .readStream
+    val dfInput = spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "spark-master-1:6667")
-      .option("subscribe", IN_TOPIC)
-      .option("maxOffsetsPerTrigger", 200)
-      .load
+      .option("subscribe", "vladimir_cherny")
+      .load()
 
-    // val value_cols = List(
-    //     "event_type",
-    //     "category",
-    //     "item_id",
-    //     "item_price",
-    //     "uid",
-    //     "timestamp"
-    // )
+    var df = dfInput.selectExpr("CAST(value AS STRING)")
 
-    // def parse_json_value (sdf: DataFrame, schema: List[String]): DataFrame = {
+    val schema = StructType(Seq(
+      StructField("category", StringType, true),
+      StructField("event_type", StringType, true),
+      StructField("item_id", StringType, true),
+      StructField("item_price", StringType, true),
+      StructField("timestamp", LongType, true),
+      StructField("uid", StringType, true)
+    ))
 
-    //     var parsedSdf = sdf.select('value.cast("string")).as[String]
-    //             .withColumn("value", regexp_replace('value, "[{]", ""))
-    //             .withColumn("tokens", split('value, ","))
-    //             .select('tokens)
+    df = df.withColumn("jsonData", from_json(col("value"), schema)).select("jsonData.*")
 
-    //     val regexp_str = "\"|}|:| |"
-    //     val ind_schema = schema.zipWithIndex.toMap
+    df = df.withColumn("date", ($"timestamp" / 1000).cast(TimestampType))
 
-    //     for (col_name <- schema)
-    //     {
-    //         parsedSdf = parsedSdf
-    //                         .withColumn(s"${col_name}", 'tokens(ind_schema(s"${col_name}")))
-    //                         .withColumn(s"${col_name}", regexp_replace(col(s"${col_name}"), regexp_str, ""))
-    //                         .withColumn(s"${col_name}", regexp_replace(col(s"${col_name}"), s"${col_name}", ""))
-    //     }
-    //     parsedSdf
-    //         .drop("tokens")
-
-    // }
-
-    val schema: StructType = StructType(
-      StructField("event_type", StringType, true) ::
-        StructField("category", StringType, true) ::
-        StructField("item_id", StringType, true) ::
-        StructField("item_price", LongType, true) ::
-        StructField("uid", StringType, true) ::
-        StructField("timestamp", LongType, true) ::
-        Nil
+    df = df.groupBy(window(col("date"), "1 hours"/*, "5 seconds"*/)).agg(
+      sum(when(col("event_type") === "buy", col("item_price")).otherwise(0)).alias("revenue"),
+      sum(when(col("uid").isNotNull, 1).otherwise(0)).alias("visitors"),
+      sum(when(col("event_type") === "buy", 1).otherwise(0)).alias("purchases")
     )
 
-    def parse_json_value (sdf: DataFrame, schema: StructType): DataFrame = {
+    df = df.withColumn("aov", col("revenue")/col("purchases"))
+    df = df.withColumn("start_ts", col("window.start").cast("long"))
+    df = df.withColumn("end_ts", col("window.end").cast("long"))
+    df = df.drop(col("window"))
 
-      var parsedSdf = sdf.select(from_json('value.cast("string"), schema).alias("value"))
-        .select(col("value.*"))
-      parsedSdf
-
-    }
-
-    val parsedSdf = parse_json_value(sdf, schema)
-      .withColumn("date", to_date(from_unixtime('timestamp / 1000)))
-      .withColumn("date", from_unixtime(unix_timestamp('date, "yyyy-MM-dd"), "yyyyMMdd"))
-
-    val res_df = parsedSdf
-      .groupBy(window(to_timestamp('timestamp), "1 hours"))
-      .agg(
-        sum(when(col("event_type") === "buy", col("item_price")).otherwise(0)).alias("revenue"),
-        sum(when(col("uid").isNotNull, 1).otherwise(0)).alias("visitors"),
-        sum(when(col("event_type") === "buy", 1).otherwise(0)).alias("purchases")
-      )
-      .select(col("window.*"), 'revenue, 'visitors, 'purchases)
-      .withColumnRenamed("start", "start_ts")
-      .withColumnRenamed("end", "end_ts")
-      .withColumn("start_ts", unix_timestamp(col("start_ts")))
-      .withColumn("end_ts", unix_timestamp(col("end_ts")))
-      .withColumn("aov", 'revenue / 'purchases)
-      .na.fill(0, Array("aov"))
-
-    // val tmpDFs = Seq(revenue, visitors, purchases)
-
-
-    // def join_mult_dfs (DFs: Seq[DataFrame]): DataFrame = {
-
-    //     var res_df: DataFrame = spark.emptyDataFrame
-    //     var tmp_df: DataFrame = spark.emptyDataFrame
-
-    //     for (ind <- 0 to DFs.length - 1)
-    //     {
-    //         if (ind == 0)
-    //             res_df = DFs(ind)
-    //         else
-    //         {
-    //             tmp_df = DFs(ind).withColumnRenamed("start_ts", "s").withColumnRenamed("end_ts", "e")
-    //             res_df = res_df.join(
-    //                 tmp_df,
-    //                 (res_df("start_ts") === tmp_df("s") && res_df("end_ts") === tmp_df("e")),
-    //                 "inner"
-    //             )
-    //             .drop("s")
-    //             .drop("e")
-    //         }
-    //     }
-    //     res_df
-
-    // }
-
-
-    // val res = join_mult_dfs(tmpDFs).withColumn("aov", 'revenue / 'purchases)
-
-    val res = res_df.toJSON.select("value")
-
-    // def createConsoleSink(df: DataFrame) = {
-    //     df
-    //     .writeStream
-    //     .outputMode("update")
-    //     .format("console")
-    //     .trigger(Trigger.ProcessingTime("5 seconds"))
-    //     //.option("checkpointLocation", s"/tmp/$USER/chk")
-    //     .option("truncate", "false")
-    //     //.option("numRows", "20")
-    // }
-
-    //val sink = createConsoleSink(res_df)
-    //val sq = sink.start
-
-
-
-    val query = res
+    val query = df
+      .selectExpr("CAST(start_ts AS STRING) AS key", "to_json(struct(*)) AS value")
       .writeStream
-      .outputMode("update")
-      .format("kafka")
       .trigger(Trigger.ProcessingTime("5 seconds"))
-      .option("checkpointLocation", s"/tmp/$USER/chk")
-      .option("kafka.bootstrap.servers", "spark-master-1:6667")
-      .option("subscribe", OUT_TOPIC)
+      .format("kafka")
+      .option("checkpointLocation", "/tmp/vladimircherny1/chk")
+      .option("kafka.bootstrap.servers", "10.0.0.5:6667")
+      .option("topic", "vladimir_cherny_lab04b_out")
       .option("maxOffsetsPerTrigger", 200)
-      .start
+      .outputMode("update")
+      .start()
+
+
+    query.awaitTermination()
+
   }
 }
 
